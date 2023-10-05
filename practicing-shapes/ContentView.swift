b1ae029002ac3858bd9c3b0575e9c63eb617725b
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-04 21:38:48 -0300

    Initial Commit

---
+++ practicing-shapes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  practicing-shapes
+//
+//  Created by Christian Manzaraz on 04/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practicing-shapes/practicing_shapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  practicing_shapesApp.swift
+//  practicing-shapes
+//
+//  Created by Christian Manzaraz on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct practicing_shapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
